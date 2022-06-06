67056f9a68e274070720202c4bb5bfc9d654109b
ayah05 <84806479+ayah05@users.noreply.github.com>
2022-06-06 10:30:39 +0200

    deleted test

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -5,9 +5,7 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
       new Interface();
-      // i wrote this shit here hgjihgj
-        System.out.println("gggg");
+
     }
-    //// Test und so
 
 }
