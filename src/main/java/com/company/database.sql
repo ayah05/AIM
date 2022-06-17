ab949f8efc3b21784c58bf23a9f3f1361711273f
ayah05 <84806479+ayah05@users.noreply.github.com>
2022-06-17 19:27:58 +0200

    inserted some information to the sql tables

--- src/main/java/com/company/database.sql
+++ src/main/java/com/company/database.sql
@@ -9,7 +9,7 @@ CREATE TABLE "Patient" (
                            "Condition" varchar(5) REFERENCES "Condition",
                            "Name" varchar(50) NOT NULL,
                            "Age" int,
-                           "Weight" int
+                           "Weight" real
 );
 
 CREATE TABLE "Condition" (
@@ -30,4 +30,36 @@ CREATE TABLE "Interaction" (
                                "Condition" varchar(5) REFERENCES "Condition",
                                "Hint" varchar(50)
 );
-
+--filling in patient names
+INSERT INTO "Patient" VALUES (Default,'','','Brigitta Haggis',34,65);
+INSERT INTO "Patient" VALUES (Default,'','','Giulietta Cirlos',23,55);
+INSERT INTO "Patient" VALUES (Default,'','','Alexio Jarlmann',50,88);
+INSERT INTO "Patient" VALUES (Default,'','','Georgiana Labdon',39,70);
+INSERT INTO "Patient" VALUES (Default,'','','York Kingscott',27,76);
+INSERT INTO "Patient" VALUES (Default,'','','Tim Bettenson',20,65);
+INSERT INTO "Patient" VALUES (Default,'','','Mahmud Witling',65,82);
+INSERT INTO "Patient" VALUES (Default,'','','Rosana Jantet',45,68);
+INSERT INTO "Patient" VALUES (Default,'','','Kathe Camm',70,57);
+INSERT INTO "Patient" VALUES (Default,'','','Janett Harris',30,60);
+INSERT INTO "Patient" VALUES (Default,'','','Brigitta Haggis',34,65);
+INSERT INTO "Patient" VALUES (Default,'','','Alexander Helks',44,90);
+INSERT INTO "Patient" VALUES (Default,'','','Martha DeLarosa',50,63);
+--filling in drugs and atc codes
+INSERT INTO "Drug" VALUES ('N01AX10','Propofol');
+INSERT INTO "Drug" VALUES ('N01AH01','Fentanyl');--N01AH - opioid anesthetics
+INSERT INTO "Drug" VALUES ('N01AH03','Sufentanil');--N01A (Allgemeinanästhesie);N01B(Lokalanästhetika)
+INSERT INTO "Drug" VALUES ('N01AX03 ','Ketamine');--N01AX - other general anesthetics
+INSERT INTO "Drug" VALUES ('N05CD08','Midazolam'); -- N05C - hypnotics and sedatives
+INSERT INTO "Drug" VALUES ('M03AB01','Suxamethonium');--M03A - muscle relaxants, peripherally acting
+INSERT INTO "Drug" VALUES ('M03AC09','Rocuronium Bromide');
+INSERT INTO "Drug" VALUES ('N01BB02','Lidocaine'); --N01B Anesthetics, Local
+INSERT INTO "Drug" VALUES ('N01AB08','Sevoflurane');
+INSERT INTO "Drug" VALUES ('N05BA01','Diazepam');--N05BA Anxiolytics, Benzodiazepine derivates
+--filling in several conditions
+INSERT INTO "Condition" VALUES ('J46','Status asthmaticus');
+INSERT INTO "Condition" VALUES ('G71','Primary Myopathies');
+INSERT INTO "Condition" VALUES ('J95','Intraoperative and postprocedural complications and disorders of respiratory system, not elsewhere classified.');
+INSERT INTO "Condition" VALUES ('I95','Hypotension'); -- contraindication to propofol
+INSERT INTO "Condition" VALUES('Z91.012','Allergy to eggs');-- contraindication to propofol
+INSERT INTO "Condition" VALUES ('T20','Burn and Corrosion of Head, Face, and Neck');--contraindication to suxamethonium (=succinylcholine)
+INSERT INTO "Condition" VALUES ('Z33','Pregnant');--idk if z33 is the right code
