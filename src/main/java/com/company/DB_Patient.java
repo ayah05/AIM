57e5c3edf4b2eff969d27c5728f660242f85c4c3
moritz <kulturaffe@gmx.at>
2022-06-19 11:53:46 +0200

    added classes for Patient-model in DB and for parsing FHIR-IFUs

---
+++ src/main/java/com/company/DB_Patient.java
@@ -0,0 +1,71 @@
+package com.company;
+
+/**
+ * This class provides a Patient object according to the data-model of our postgreSQL-DB <p>
+ *     Weight is in kilograms!
+ */
+public class DB_Patient {
+    private String[] conditions;
+    private String[] drugs;
+    private String name;
+    private int age;
+    private double weight;
+
+    public DB_Patient(){}
+    public DB_Patient(String name, String[] conditions, String[] drugs, int age, double weight){
+        this.name = name; this.conditions = conditions; this.drugs = drugs; this.age = age;
+        this.weight = weight;
+    }
+    public DB_Patient(String name, String[] conditions, String[] drugs, int age){
+        this.name = name; this.conditions = conditions; this.drugs = drugs; this.age = age;
+    }
+    public DB_Patient(String name, String[] conditions, String[] drugs){
+        this.name = name; this.conditions = conditions; this.drugs = drugs;
+    }
+    public String[] getConditions(){
+        return this.conditions;
+    }
+    public void setConditions(String[] cond){
+        if(this.conditions.length<1){
+            this.conditions = cond;
+        }else{ // if there are already conditions, add the new ones instead of overriding old ones.
+            int new_len = this.conditions.length + cond.length;
+            String[] result = new String[new_len];
+            System.arraycopy(this.conditions, 0, result, 0, this.conditions.length);
+            System.arraycopy(cond, 0, result, this.conditions.length, cond.length);
+            this.conditions = result;
+        }
+    }
+    public String[] getDrugs(){
+        return this.drugs;
+    }
+    public void setDrugs(String[] drgs){
+        if(this.drugs.length<1){
+            this.drugs = drgs;
+        }else{ // if there are already drugs, add the new ones instead of overriding old ones.
+            int new_len = this.drugs.length + drgs.length;
+            String[] result = new String[new_len];
+            System.arraycopy(this.drugs, 0, result, 0, this.drugs.length);
+            System.arraycopy(drgs, 0, result, this.drugs.length, drgs.length);
+            this.drugs = result;
+        }
+    }
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String new_name){
+        this.name = new_name;
+    }
+    public int getAge(){
+        return this.age;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public double getWeight(){
+        return this.weight;
+    }
+    public void setWeight(double weight){
+        this.weight = weight;
+    }
+}
---
+++ src/main/java/com/company/FHIR_IPS_parser.java
@@ -0,0 +1,7 @@
+package com.company;
+
+/**
+ * this class takes IPS-FHIR-bundle-documents and parses them in order to provide a patient ressource for our database (Name, Age, Drugs taken, preconditions and maybe weight)
+ */
+public class FHIR_IPS_parser {
+}
