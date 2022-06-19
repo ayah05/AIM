499f2649a14a9370a9d249ed4679c9227efa529b
moritz <kulturaffe@gmx.at>
2022-06-19 16:14:19 +0200

    FHIR-parser basics

--- src/main/java/com/company/FHIR_IPS_parser.java
+++ src/main/java/com/company/FHIR_IPS_parser.java
@@ -1,7 +1,15 @@
 package com.company;
+import ca.uhn.fhir.context.FhirContext;
+import org.hl7.fhir.r4.model.*;
 
 /**
  * this class takes IPS-FHIR-bundle-documents and parses them in order to provide a patient ressource for our database (Name, Age, Drugs taken, preconditions and maybe weight)
  */
 public class FHIR_IPS_parser {
+    FhirContext ctx;
+    public FHIR_IPS_parser(){
+        ctx = FhirContext.forR4();
+    }
+
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module com.company {
     requires javafx.controls;
     requires javafx.fxml;
+    requires hapi.fhir.base;
+    requires hapi.fhir.structures.r4;
 
 
     opens com.company to javafx.fxml;
