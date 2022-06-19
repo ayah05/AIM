e561139f3c5c436505af5bc7524a39153b066c88
moritz <kulturaffe@gmx.at>
2022-06-19 15:56:45 +0200

    jfx works finallyyyyy

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(TestController.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(TestController.class.getResource("/hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
