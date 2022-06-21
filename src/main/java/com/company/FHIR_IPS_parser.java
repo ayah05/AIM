0e4cda35012a898c3e32f8d0a6fe26953fbb8c19
moritz <kulturaffe@gmx.at>
2022-06-21 17:48:46 +0200

    added active problems to conditions

--- src/main/java/com/company/FHIR_IPS_parser.java
+++ src/main/java/com/company/FHIR_IPS_parser.java
@@ -20,7 +20,7 @@ public class FHIR_IPS_parser {
         ctx = FhirContext.forR4();
     }
     public /* DB_Patient */ boolean readIPS(String pathName){ // "./src/main/IPS-example-Bundle-with-renal-disease-et-al.json"
-        String name = null; List<String> conditions = new ArrayList<>(), drugs = new ArrayList<>(); int age = -1;
+        String name = null; List<String> conditions = new ArrayList<>(), drugs = new ArrayList<>(); int age = -1; Date dob;
         boolean worked = false;
         IParser parser = ctx.newJsonParser();
         File ips_file = new File(pathName);
@@ -32,11 +32,12 @@ public class FHIR_IPS_parser {
             Patient pat = BundleUtil.toListOfResourcesOfType(ctx,ips_bundle,Patient.class).get(0);
             name = pat.getName().get(0).getGivenAsSingleString() +" " + pat.getName().get(0).getFamily();
             age = (int)((System.currentTimeMillis() - pat.getBirthDate().getTime()) / 3.154e+10); // subtracts the birthdate in ms from currentTime in ms, divides by the approx # of ms in a year and cuts decimal digits
+            dob = pat.getBirthDate();
             // drugs
             for (MedicationStatement ms : BundleUtil.toListOfResourcesOfType(ctx,ips_bundle, MedicationStatement.class)) {
                 if (ms.hasStatus() && ms.getStatus().equals(MedicationStatement.MedicationStatementStatus.ACTIVE)) {
                     Medication med = (Medication) ms.getMedicationReference().getResource();
-                    if (med.hasCode()) {
+                    if (med.hasCode() && med.getCode().hasCoding()) {
                         List<Coding> codeList = med.getCode().getCoding();
                         for (Coding code : codeList) {
                             if (code.hasSystem() && code.hasCode() && (code.getSystem().equals("http://www.whocc.no/atc") || code.getSystem().equals("urn:oid:2.16.840.1.113883.6.73"))) {
@@ -46,12 +47,29 @@ public class FHIR_IPS_parser {
                     }
                 }
             }
-            // conditions
-
+            // conditions (active problems)
+            if(ips_bundle.hasEntry()) {
+                for(Composition.SectionComponent sectComp : BundleUtil.toListOfResourcesOfType(ctx,ips_bundle, Composition.class).get(0).getSection()){
+                    if(sectComp.hasCode() && sectComp.getCode().hasCoding() && sectComp.getCode().getCoding().get(0).getCode().equals("11450-4")){ //"11450-4" -- active problems
+                        for(Reference ref : sectComp.getEntry()){
+                            Condition cond = (Condition) ref.getResource();
+                            if(cond.hasCode() && cond.getCode().hasCoding()){
+                                List<Coding> codeList = cond.getCode().getCoding();
+                                for(Coding code : codeList){
+                                    if(code.hasCode() && code.hasSystem() && (code.getSystem().contains("http://hl7.org/fhir/sid/icd-10")  || code.getSystem().equals("urn:oid:2.16.840.1.113883.6.3") /*general ICD-10*/ || code.getSystem().equals("urn:oid:2.16.840.1.113883.6.90") /*USA*/ || code.getSystem().equals("urn:oid:2.16.840.1.113883.6.3.2") /*NL*/ || code.getSystem().equals("http://fhir.de/CodeSystem/dimdi/icd-10-gm") || code.getSystem().contains("urn:oid:1.2.276.0.76.5.") /*DE -- leider alle kodierschemata weil jede jährliche aktualisierung eine neue OID kriegt :/*/)){
+                                        conditions.add(code.getCode().toUpperCase(Locale.ROOT));
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
             // allergyIntolerances -> conditions
 
             // test output:
-            System.out.println(drugs);
+
+            System.out.println( conditions);
             worked = true;
         } catch (IOException e) {
             e.printStackTrace();
