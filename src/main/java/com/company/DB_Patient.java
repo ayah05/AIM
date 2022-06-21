d96ae12110a25254cd903097b991ed92a3e5f53c
moritz <kulturaffe@gmx.at>
2022-06-21 20:46:23 +0200

    added example patient constructor: DB_Patient(boolean test) --doesn't matter if true or false # used localDate instead of deprecated Date constructor

--- src/main/java/com/company/DB_Patient.java
+++ src/main/java/com/company/DB_Patient.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -18,6 +21,9 @@ public class DB_Patient {
     private int patID_DB_PK= -1;
 
     public DB_Patient(){
+    }
+    public DB_Patient(boolean test){
+       this.name = "Martha DeLarosa"; this.age = 50; this.dob = Date.from(LocalDate.of(1972, Month.MAY,1).atStartOfDay(ZoneId.of("Europe/Amsterdam")).toInstant()); this.conditions.addAll(List.of("N18.9","N95.1","Z88.0")); this.drugs.addAll(List.of("L02BG03","G02CX04","J01FA09", "B01AA03")); this.weight=63;
     }
     public DB_Patient(String name, List<String> conditions, List<String> drugs, Date dob, double weight, int age){
         this.name = name; this.conditions = conditions; this.drugs = drugs; this.dob = dob;
