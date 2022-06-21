a4dc44f8f1a424414727abbc26955d0ffc58399f
moritz <kulturaffe@gmx.at>
2022-06-21 23:52:29 +0200

    added test for DB_Patient test constructor, even though unnecessary now that the IPS parser is done ^^

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -18,11 +18,13 @@ public class Main extends Application {
         /* flo test */
         launch();
 
+
         /* mo test
         FHIR_IPS_parser parse = new FHIR_IPS_parser();
         System.out.println(parse.readIPS("./src/main/IPS-example-Bundle-with-renal-disease-et-al.json"));
-         */
-        //
+
+        System.out.println(new DB_Patient(true)); // for testing the test DB_Patient: works just as well as readIPS on our example IPS :D
+        */
 
         /* ayah test
 
