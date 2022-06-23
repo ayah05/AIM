2f942f2d8aa668df92d6c91056b75ea4d62fb3f5
ayah05 <84806479+ayah05@users.noreply.github.com>
2022-06-23 10:47:48 +0200

    edited function listingAllPatients in SQLtoJava to return void instead of String

--- src/main/java/com/company/Main.java
+++ src/main/java/com/company/Main.java
@@ -28,13 +28,13 @@ public class Main extends Application {
         FHIR_IPS_parser parse = new FHIR_IPS_parser();
         System.out.println(parse.readIPS("./src/main/IPS-example-Bundle-with-renal-disease-et-al.json"));
         System.out.println(new DB_Patient(true)); // for testing the test DB_Patient: works just as well as readIPS on our example IPS :D
-        // shows all terminals: */
+        // shows all terminals:
         System.out.println("All terminals: "+ ReadEcardGeneric.getAllTerminals());
         // shows all terminals with card present (maybe easier just to make the user coose from these)
         System.out.println("Terminals with card present: " + ReadEcardGeneric.getAllTerminalsWithCardPresent());
         // reads the ecard in terminal 0, true just puts out debug-info
         System.out.println("Created DB_Patient:\n"+ReadEcardGeneric.readCard(0,true));
-
+        */
 
 
 
@@ -42,7 +42,7 @@ public class Main extends Application {
             SQLtoJava test = new SQLtoJava();
             Connection connection = test.setConnection();
             test.listingAllPatients(connection);
-         */
+*/
     }
 
 
--- src/main/java/com/company/SQLtoJava.java
+++ src/main/java/com/company/SQLtoJava.java
@@ -8,10 +8,67 @@ import java.sql.*;
 import java.util.ArrayList;
 
 public class SQLtoJava {
-     /*  ArrayList <DB_Patient> patientlist = new ArrayList<DB_Patient>();
+    private static String url = "jdbc:postgresql://localhost:5432/AIM";
+    private static String user = "postgres";
+    private static String password = "medProjekt";
+    private static Connection connection;
+
+    public SQLtoJava() {
+    }
+
+    public Connection setConnection(){
         try {
-            Class.forName("org.postgresql");
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }*/
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection to the database"+url+" is successful");
+            return connection;
+        } catch (SQLException | ClassNotFoundException e) {
+            System.out.println("Something went wrong. Please try again to connect to the Database.");
+        }
+        return null;
+    }
+
+    public void listingAllPatients(Connection connection){
+        try{
+            ArrayList <DB_Patient> patientlist = new ArrayList<>();
+            String query = "SELECT *FROM \"Patient\"";
+            Statement statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            DB_Patient patient;
+            while (rs.next()){
+                patient = new DB_Patient(rs.getString("Name"),rs.getString("Condition"),rs.getString("Drug"),rs.getDouble("Weight"),rs.getInt("PatientID"),rs.getInt("Age"));
+                patientlist.add(patient);
+                for(DB_Patient p: patientlist){
+                    System.out.println(p.toStringWithoutDOB()+"\n");
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+
+/*
+try {
+        ArrayList<DB_Patient> patientlist = new ArrayList<>();
+        Class.forName("org.postgresql.Driver");
+        Connection connection = DriverManager.getConnection(url, user, password);
+
+        String query = "SELECT *FROM \"Patient\"";
+        Statement statement = connection.createStatement();
+        ResultSet rs = statement.executeQuery(query);
+        DB_Patient patient;
+        //
+        while (rs.next()){
+            patient = new DB_Patient(rs.getString("Name"),rs.getString("Condition"),rs.getString("Drug"),rs.getDouble("Weight"),rs.getInt("PatientID"),rs.getInt("Age"));
+            patientlist.add(patient);
+            for(DB_Patient p: patientlist){
+                System.out.println(p.toStringWithoutDOB()+"\n");
+            }
+        }
+    } catch (SQLException | ClassNotFoundException throwables) {
+        throwables.printStackTrace();
+    }
+*/
+
 }
