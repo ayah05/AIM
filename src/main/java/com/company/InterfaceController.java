ffaa6ca3d6d9d22b01c087a634111e9c016ea4b4
moritz <kulturaffe@gmx.at>
2022-06-26 21:43:15 +0200

    added interface for sql2java classes

--- src/main/java/com/company/InterfaceController.java
+++ src/main/java/com/company/InterfaceController.java
@@ -28,7 +28,7 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class InterfaceController implements Initializable {
     /// TODO: sein eigenes SQL-pw eingeben.....
-    public SQLtoJava connection = new SQLtoJava("0");
+    public SQLtoJava connection = new SQLtoJava("sql");
 
     @FXML
     private Button B_Anamnese,saveButton,importJason;
@@ -115,7 +115,7 @@ public class InterfaceController implements Initializable {
     // needet for choiceBoxes and ListView
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) { // bis 258!
-        ////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /////////////////////////////////////////////////////
         //Test Data
         /////////////////////////////////////////////////////
 
@@ -346,8 +346,10 @@ public class InterfaceController implements Initializable {
             if(Patient.getDOB() != null){
                 DP.setValue(Patient.getDOB().toInstant().atZone(ZoneId.systemDefault()).toLocalDate());
             }
-
+            currentPatientMedication.addAll(Patient.getDrugs());
+            patMedListView.getItems().addAll(currentPatientMedication);
 
         }
     }
+
 }
